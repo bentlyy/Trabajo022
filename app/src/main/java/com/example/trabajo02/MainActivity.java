9a36ad8b775f4a4a1e4b74a26015594968591969
Alejandro <garayaa0606@gmail.com>
2022-05-06 17:37:31 -0400

    Version-01

--- app/src/main/java/com/example/trabajo02/MainActivity.java
+++ app/src/main/java/com/example/trabajo02/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.trabajo02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+ private EditText et1,et2;
+ private TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1=findViewById(R.id.et1);
+        et2=findViewById(R.id.et2);
+        tv1=findViewById(R.id.tv1);
+
     }
+    public void sumar(view v){}
 }
